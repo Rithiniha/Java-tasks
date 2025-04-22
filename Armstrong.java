5606bcab72ef6ad76fc724d6d8f41c1fb8d99ced
Rithiniha kamalanathan <164982399+Rithiniha@users.noreply.github.com>
2025-04-22 23:36:47 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -1,11 +1,27 @@
 package armstrong;
 
+import java.util.Scanner;
+
 public class Armstrong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        int original = number;
+        int result = 0;
 
-	public static void main(String[] args) {
-	
-		
+        while (number != 0) {
+            int digit = number % 10;
+            result += digit * digit * digit;
+            number /= 10;
+        }
 
-	}
+        if (original == result) {
+            System.out.println(original + " is an Armstrong number.");
+        } else {
+            System.out.println(original + " is not an Armstrong number.");
+        }
 
+        sc.close();
+    }
 }
